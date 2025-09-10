e2a0e1d92797299055625733103caa2f6d157d40
taibv.it@gmail.com <taibv.it@gmail.com>
2025-09-10 16:38:58 +0700

    Update

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "🔄 Building Piston from source..."
+echo "🔄 Building Piston from source (no cache)..."
 
 docker-compose down -v
-docker-compose up -d --build api
+
+# Build lại không dùng cache
+docker-compose build --no-cache api
+
+# Chạy lại container
+docker-compose up -d api
+
 echo "🔄 Following logs ..."
 docker logs -f piston_api
